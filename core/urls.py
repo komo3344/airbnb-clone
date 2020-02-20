db3408fc0d062dda9ab66712a004ad95235cafd4
komo3344 <jy940608@naver.com>
2020-02-21 03:40:32 +0900

    Class Base View

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from rooms import views as room_views
 
 app_name = "core"  # namespace랑 같아야함
 
-urlpatterns = [path("", room_views.all_rooms, name="home")]  # view에 이름주기
+urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]  # view에 이름주기
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,22 @@
-from django.core.paginator import Paginator, EmptyPage
-from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(
-        room_list, 10, orphans=5
-    )  # 5와 같거나 작으면 이전페이지에서 보여줌 6이상이면 다음페이지에서 보여줌
+class HomeView(ListView):
 
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/all_rooms.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+    """ HomeView Definition """
 
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+    # page_kwarg = "potato"  # /?page=* -> /?potato=*
