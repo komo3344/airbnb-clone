c4cbe51ba5972a2e8f88d650a0ad93a27b4db2d1
komo3344 <jy940608@naver.com>
2020-02-23 23:12:51 +0900

    Finising the form

--- rooms/views.py
+++ rooms/views.py
@@ -33,9 +33,45 @@ class RoomDetail(DetailView):
 def Search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guest = int(request.GET.get("guest", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
     room_types = models.RoomType.objects.all()
-    return render(
-        request,
-        "rooms/search.html",
-        context={"city": city, "countries": countries, "room_types": room_types},
-    )
+    amenities = models.Amenity.objects.all()
+    Facilities = models.Facility.objects.all()
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    print(s_amenities, s_facilities)
+    # 입력받는 것
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guest": guest,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    # DB에서 가져오는 것
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "Facilities": Facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices})
+
