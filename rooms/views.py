cc6e707679b9f6675bffb2cfd1e9974b74fb0d0a
komo3344 <jy940608@naver.com>
2020-02-23 21:42:44 +0900

    starting the form

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django_countries import countries
 from django.shortcuts import render
-from django.http import Http404
 from . import models
 
 
@@ -28,3 +28,14 @@ class RoomDetail(DetailView):
     """ RoomDetail Definition """
 
     model = models.Room
+
+
+def Search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        context={"city": city, "countries": countries, "room_types": room_types},
+    )
