ed0667891b22f435b73db1a1200b2c768acde514
komo3344 <jy940608@naver.com>
2020-02-26 09:23:58 +0900

    Add Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView, View
-from django_countries import countries
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from . import models, forms
 
@@ -34,6 +34,7 @@ class SearchView(View):
     def get(self, request):
 
         country = request.GET.get("country")
+
         if country:
 
             form = forms.SearchForm(request.GET)
@@ -88,11 +89,20 @@ class SearchView(View):
                 for facility in facilities:
                     filter_args["facilities"] = facility
 
-                rooms = models.Room.objects.filter(**filter_args)
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
 
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get("page", 1)
+
+                rooms = paginator.get_page(page)
+
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
         else:
 
             form = forms.SearchForm()
 
-        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+        return render(request, "rooms/search.html", {"form": form})
 
