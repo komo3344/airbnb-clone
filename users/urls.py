70c36f25e5d62ec418ee42bab201a34fc6613fcd
komo3344 <jy940608@naver.com>
2020-03-16 03:13:04 +0900

    Adding info to the sessions

--- users/urls.py
+++ users/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
     path("update-passwod/", views.UpdatePasswordView.as_view(), name="password"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
+    path("switch-hosting/", views.switch_hosting, name="switch-hosting"),
 ]
 
--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required
 from . import forms, models, mixins
 
 
@@ -263,3 +264,12 @@ class UpdatePasswordView(
 
     def get_success_url(self):
         return self.request.user.get_absolute_url()
+
+
+@login_required
+def switch_hosting(request):
+    try:
+        del request.session["is_hosting"]
+    except KeyError:
+        request.session["is_hosting"] = True
+    return redirect(reverse("core:home"))
