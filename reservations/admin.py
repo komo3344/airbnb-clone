86835339c3e1f998cf61021cf597af2a68074a31
komo3344 <jy940608@naver.com>
2020-03-17 22:59:28 +0900

    달력 날짜 예약

--- reservations/admin.py
+++ reservations/admin.py
@@ -16,3 +16,8 @@ class ReservationAdmin(admin.ModelAdmin):
         "is_finished",
         "guest",
     )
+
+
+@admin.register(models.BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+    list_display = ("day", "reservation")
